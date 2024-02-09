72aeba2f5f4a397e72cb38077997b3ff17983154
Sahej Singh <sahej3526@gmail.com>
2024-02-09 04:16:48 -0600

    module

---
+++ code/__init__.py
@@ -0,0 +1,38 @@
+class AClass:
+    """isaclass"""
+
+
+def ireturnaclass():
+    """isafunction
+
+    Returns:
+        AClass: A class
+    """
+    return AClass()
+
+
+def ireturnbclass():
+    """isafunction
+
+    Returns:
+        (AClass): A class
+    """
+    return AClass()
+
+
+def ireturncclass():
+    """isafunction
+
+    Returns:
+        amod.AClass: A class
+    """
+    return AClass()
+
+
+def ireturndclass():
+    """isafunction
+
+    Returns:
+        "AClass": A class
+    """
+    return AClass()
